1a1b6341528e13ced009fe91eb8ca8712231e3a1
Isaac Alger <ialger@kimonocloud.com>
2018-09-06 21:05:19 -0600

    added seqeulize initialization and created a student model. added add route with working students creation in the DB

--- server.js
+++ server.js
@@ -1,29 +1,14 @@
 const express = require('express');
 const path = require('path');
-const sequelize = require('sequelize');
-const conn = new sequelize('abcdatatracker', 'isaacalger', 'I7410123', {
-        host: 'localhost',
-        dialect: 'postgres',
-        operatorsAliases: false,
-    
-        pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-        },
-    });
-
-conn.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+const Student = require('./models').Student;
+//const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
 
 //Init app
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -31,50 +16,6 @@ app.set('view engine', 'pug');
 
 //Home Route
 app.get('/', (req, res) => {
-    let entries = [
-        {
-            id: 1,
-            student: 'Boston',
-            teacher: 'Ms. Megan',
-            time: '10:32 AM',
-            date: '5/6/17', 
-            behavior:[
-                {
-                    id: 1,
-                    description: 'Student forces hand down throat',
-                },
-                {
-                    id: 2,
-                    description: 'Flops on ground',
-                },
-            ],
-
-            antecedent:[1,2],
-            consequence:[3,4],
-            location: 'classroom cubby'          
-        },
-        {
-            id: 2,
-            student: 'Izo',
-            teacher: 'Ms. Rylee',
-            time: '10:32 AM',
-            date: '5/6/17', 
-            behavior:[
-                {
-                    id: 1,
-                    description: 'Student bites arm',
-                },
-                {
-                    id: 2,
-                    description: 'Flops on ground',
-                },
-            ],
-
-            antecedent:[1,2],
-            consequence:[3,4],
-            location: 'classroom cubby'          
-        },
-    ];
 
     res.render('index', {
         title:'ABC Data',
@@ -82,11 +23,47 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/add', (req, res) => {
+    res.render('add', {
+        title: 'Add Objects'
+    })
+})
+
 app.get('/abc_entry/add', (req, res) => {
     res.render('add_entry', {
         title:'ABC Data Form'
     });
 });
+
+app.post('/student/add', (req, res) => {
+    Student.create(req.body)
+        .then(() => res.redirect('/add'));
+});
+
+/*
+// Init Database Connection
+let sequelize = new Sequelize('abcdatatracker', 'isaacalger', 'I7410123', {
+    host: 'localhost',
+    dialect: 'postgres',
+    operatorsAliases: false,
+
+    pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+    },
+});
+
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+*/
 
 // Start Server
 app.listen(3000, () => {
